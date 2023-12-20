2358bf7d700442fd82fa43980351e0c52a782ecb
taloy2 <sinabiananrey@gmail.com>
2023-12-20 09:08:10 +0800

    final spa

---
+++ app.js
@@ -0,0 +1,16 @@
+function navigateTo(page) {
+    // Hide all content sections
+    const contentSections = document.querySelectorAll('main');
+    contentSections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected content section
+    const selectedSection = document.getElementById(page);
+    selectedSection.style.display = 'block';
+}
+
+// Initial navigation to the home page
+navigateTo('home');
+// app.js
+
